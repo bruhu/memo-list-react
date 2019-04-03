578c6aefaec28a020b5c1c25228d2c86cfb6a788
Bru <annabru.abh@gmail.com>
2019-04-03 13:47:05 +0200

    Updates

---
+++ precache-manifest.2a49f62807d2a6f47834c381c08ba0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83df8e0794f7ba863679",
+    "url": "/memo-list-react/static/css/main.924f57de.chunk.css"
+  },
+  {
+    "revision": "83df8e0794f7ba863679",
+    "url": "/memo-list-react/static/js/main.d571462b.chunk.js"
+  },
+  {
+    "revision": "fc265edd4002b10a09aa",
+    "url": "/memo-list-react/static/js/runtime~main.f01de901.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/memo-list-react/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memo-list-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a7ee208c7f67e7421d9995fb27dfb0a0",
+    "url": "/memo-list-react/index.html"
+  }
+];
